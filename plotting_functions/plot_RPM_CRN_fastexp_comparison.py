7b85e4ae8af90b0f136f8a254e637084fa8d1efe
Martin Hurst <Martin.Hurst@glasgow.ac.uk>
2019-07-29 17:08:14 +0100

    minor update to plotting script

--- plotting_functions/plot_RPM_CRN_fastexp_comparison.py
+++ plotting_functions/plot_RPM_CRN_fastexp_comparison.py
@@ -11,8 +11,8 @@ March 7th 2016
 """
 
 # import plotting tools and set the back end for running on server
-import matplotlib
-matplotlib.use('Agg')
+#import matplotlib
+#matplotlib.use('Agg')
 
 #import modules
 import numpy as np
@@ -27,8 +27,17 @@ rc('xtick.major',pad=1)
 padding = 1
 
 #create blank figure
-plt.figure(1,figsize=(6.6,6.6))
+plt.figure(1,figsize=(6.6,9.9))
 
+# spatial plot
+ax1 = plt.subplot(311)
+plt.axis('equal')
+
+# concentration plot
+ax2 = plt.subplot(312)
+ax3 = plt.subplot(313)
+
+    
 ProfileFileList = ["../driver_files/" + "SlowExp_ShoreProfile.xz", "../driver_files/" + "FastExp_ShoreProfile.xz"]
 ConcentrationFileList = ["../driver_files/" + "SlowExp_Concentrations.xn", "../driver_files/" + "FastExp_Concentrations.xn"]
 ColourMaps = [cm.bone_r,cm.Reds]
@@ -54,19 +63,17 @@ for i in range(0,len(ProfileFileList)):
     # Get info on vertical from header
     Header = np.array(Lines[0].strip().split(" "),dtype=np.float)
     CliffHeight = Header[0]
-    dz = Header[1]
+    print(CliffHeight)
+    dz = Header[2]
 
     # Only plot every so many years
+    StartTime = 10000
     PlotTime = StartTime
     PlotInterval = -1000
     EndTime = 0
-
-    # spatial plot
-    ax1 = plt.subplot(211)
-    plt.axis('equal')
     
     #Get header info and setup X coord
-    for j in range(1,NoLines-1):
+    for j in range(2,NoLines-1):
         
         Line = (Lines[j].strip().split(" "))
         Time = float(Line[0])
@@ -78,22 +85,19 @@ for i in range(0,len(ProfileFileList)):
                 
         if (Time == StartTime) or (Time == -9999):
             print(Time)
-            ax1.plot(X,Z,'k--',lw=1.,zorder=10,label="Initial Profile")
+            ax1.plot(X+i,Z,'k--',lw=1.,zorder=10,label="Initial Profile")
             PlotTime += PlotInterval
         elif Time <= EndTime:
-            ax1.plot(X,Z,'k-',lw=1.,zorder=10,label="Final Profile")
+            ax1.plot(X+i,Z,'k-',lw=1.,zorder=10,label="Final Profile")
             PlotTime += PlotInterval
             break
         elif (Time <= PlotTime):
             print(Time)
             colour =(Time-StartTime)/(EndTime-StartTime)
-            ax1.plot(X,Z,'-',lw=1.,color=ColourMaps[i]((Time)/(StartTime)))
+            ax1.plot(X+i,Z,'-',lw=1.,color=ColourMaps[i]((Time)/(StartTime)))
             PlotTime += PlotInterval
 
     ax1.plot(X,Z,'k-',lw=1.5)
-
-    # concentration plot
-    ax2 = plt.subplot(212)
 
     N10Line = (ConcLines[-1].strip().split(" "))   #reading line -2?   
     N10 = np.array(N10Line[1:],dtype="float64")
@@ -103,17 +107,25 @@ for i in range(0,len(ProfileFileList)):
     X2 = X2[mask]
            
     ax2.plot(X2,N10,'-',lw=1.5, color=ColourMaps[i](1.))
-
     
+    if i == 0:
+        N10old = N10
+    
+ax3.plot(X2,N10old-N10,'k-')
+
 # tweak the plot
 ax1.set_xticklabels([])
-ax2.set_xlabel("Distance (m)")
+ax2.set_xticklabels([])
+ax3.set_xlabel("Distance (m)")
 ax1.set_ylabel("Elevation (m)")
 ax2.set_ylabel("$^{10}$Be concentration (at g$^{-1}$)")
+ax3.set_ylabel("Difference in $^{10}$Be concentration (at g$^{-1}$)")
+ax1.set_xlim(0,175)
+ax2.set_xlim(0,175)
+ax3.set_xlim(0,175)
 
-ax1.set_ylim(-CliffHeight/2,CliffHeight/2)
 #plt.ylim(-30,30)
-plt.tight_layout()
+#plt.tight_layout()
 plt.savefig('RPM_test_fast_exp.png',dpi=300)
 
         
