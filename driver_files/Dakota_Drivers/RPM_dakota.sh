c231e041b0dd9290ee05905fc0d5f01472968e53
Jenny Shadrick <jrs17@ese-ajs13.ese.ic.ac.uk>
2019-08-27 16:11:20 +0100

    Added dakota RPM files

---
+++ driver_files/Dakota_Drivers/RPM_dakota.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dakota -i RPM_dakota.in -o RPM_dakota.out -write_restart RPM_dakota.rst &> RPM_dakota.log
---
+++ driver_files/Dakota_Drivers/RPM_driver.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 13:23:06 2019
+
+@author: lguest
+"""
+
+# Import  modules
+import sys
+import subprocess
+from subprocess import call
+from yaml import safe_load
+
+# Import RPM model 
+subprocess.Popen(["RPM_dakota.out"])
+
+# Set physical constants as used by RPM - these are values read in command line
+
+Gradient = 1
+TidalRange = 8
+SubtidalEfficacy = 0.005
+WaveAttenuationConst = 0.01
+
+#########################################
+#                                       #
+#    Step 1: Use Dakota created         #
+#    input files to prepare for         #
+#    model run.                         #
+#                                       #
+#########################################
+
+input_template = "input_template.yml"
+inputs = "inputs.yml"
+call(["dprepro", sys.argv[1], input_template, inputs])
+call(['rm', input_template])
+
+#########################################
+#                                       #
+#    Step 2: Run Model                  #
+#                                       #
+#########################################
+
+# Load parameters from the yaml formatted input.
+with open(inputs, "r") as f:
+    params = safe_load(f)
+    Resistance = params["Resistance"]
+    WeatheringRate = params["WeatheringRate"]
+  
+# set up command to run model 
+  
+Launchstr = " ./RPM_dakota.out /home/lguest/DAKOTA/  "+ sys.argv[2] +"  0  "+ str(Gradient) +" "+ str(TidalRange) +" "+ str(SubtidalEfficacy) +" "+ str(WaveAttenuationConst) +" "+ str(Resistance) +" "+ str(WeatheringRate) +"" 
+print(Launchstr) 
+
+#########################################
+#                                       #
+#    Step 3: Write Output in format     #
+#    Dakota expects                     #
+#                                       #
+#########################################
+
+# Each of the metrics listed in the Dakota .in file needs to be written to
+# the specified output file given by sys.argv[2]. This is how information is
+# sent back to Dakota.
+
+# RPM already produces outputs expected by dakota (argv[2])
+# Don't need to write it to expected file 
+
+# Get RMSE/ Likelihood output file from RPM_dakota 
+# RMSE/ Likelihood (objective function) currently calculated within RPM model 
+
+
+  
