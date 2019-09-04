efc3d42c31990eca23ec1271486416e627c60743
Jenny Shadrick <jrs17@ic.ac.uk>
2019-09-04 11:53:01 +0100

    changes to .py

--- driver_files/Dakota_Drivers/template_dir/RPM_driver.py
+++ driver_files/Dakota_Drivers/template_dir/RPM_driver.py
@@ -48,9 +48,9 @@ with open(inputs, "r") as f:
   
 # set up command to run model 
   
-Launchstr = "../../RPM_dakota.exe /dakotaQuesoWorking/Rocky-Profile-Model/driver_files/Dakota_Drivers/ "+ sys.argv[2] +" /dakotaQuesoWorking/Rocky-Profile-Model/driver_files/Data/CB_profile.txt 0 "+ str(Gradient) +" "+ str(TidalRange) +" "+ str(SubtidalEfficacy) +" "+ str(WaveAttenuationConst) +" "+ str(Resistance) +" "+ str(WeatheringRate)
-subprocess.Popen(Launchstr,shell=True)
-print(Launchstr) 
+#Launchstr = "../../RPM_dakota.exe /dakotaQuesoWorking/Rocky-Profile-Model/driver_files/Dakota_Drivers/ "+ sys.argv[2] +" /dakotaQuesoWorking/Rocky-Profile-Model/driver_files/Data/CB_profile.txt 0 "+ str(Gradient) +" "+ str(TidalRange) +" "+ str(SubtidalEfficacy) +" "+ str(WaveAttenuationConst) +" "+ str(Resistance) +" "+ str(WeatheringRate)
+#subprocess.Popen(Launchstr,shell=True)
+#print(Launchstr) 
 
 #########################################
 #                                       #
