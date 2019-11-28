a678720951d5d7e20328280f2f0c49ef5c17857e
Jenny Shadrick <jrs17@ic.ac.uk>
2019-11-28 10:43:52 +0000

    changes to dakota file

--- driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
+++ driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
@@ -218,8 +218,8 @@ int main(int nNumberofArgs,char *argv[])
 
     // print initial condition to file - this is for testing - remove
 	double TempTime = -9999;
-    PlatformModel.WriteProfile(OutputFileName, TempTime);			
-	if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
+    //PlatformModel.WriteProfile(OutputFileName, TempTime);			
+	//if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
 
     //Loop through time
 	while (Time >= EndTime)
@@ -266,9 +266,9 @@ int main(int nNumberofArgs,char *argv[])
 		{
 			cout.flush();
 			cout << "RPM: Time " << setprecision(2) << fixed << Time << " years\r";
-			PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
-            if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
-			PrintTime -= PrintInterval;
+			//PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
+            //if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
+			//PrintTime -= PrintInterval;
 		}
 
 		//update time
