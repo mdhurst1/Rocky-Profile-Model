1e0508c46559de58bb4f3837d8d5e12d8bc91168
Jenny Shadrick <jrs17@ic.ac.uk>
2020-01-14 17:07:14 +0000

    edits to wave dakota input file

--- plotting_functions/plot_dakota.py
+++ plotting_functions/plot_dakota.py
@@ -23,11 +23,13 @@ rc('ytick.major',pad=5)
 rc('xtick.major',pad=5)
 padding = 5
 
-#def make_plot(FileName,ColourMap):
+Folder = "/Users/jennyshadrick/Dakota_Runs/"
 
-    for filename in ('/Users/jennyshadrick/Dakota_Runs/Run_1','/Users/jennyshadrick/Dakota_Runs/Run_2'):
-        
-    
+for FileName in (Folder+'Wave_Test_1',Folder+'Wave_Test_2',Folder+'Wave_Test_3'):  #Folder+'Run_1',Folder+'Run_2',Folder+'Run_3',Folder+'Run_4'):
+
+    ColourMap = cm.gray
+
+
     #create blank figure
     plt.figure(1,figsize=(8,4))
 
@@ -45,8 +47,9 @@ padding = 5
     LastOutput = MorphLines[-1].strip().split(" ") 
     LastProfile = np.array(LastOutput[2:],dtype="float64")
     LCPosition = float(LastOutput[3])   #cliff position 2 for SY, 3 for CB - check this   
-
     LastProfile = LCPosition-LastProfile
+
+    #Need to normalise modelled topography to furthest cliff position?
     
     # Get Z Values
     HeaderLine = MorphLines[0].strip().split(" ")
@@ -89,80 +92,54 @@ padding = 5
             print(Time)
             print (np.shape(X))
             print (np.shape(Z))
-            ax1.plot(X,Z,'--',lw=1.5,color=ColourMap((Time)/(StartTime)), label='Modelled Morphology')
+            ax1.plot(X,Z,'--',lw=1.5,color=ColourMap((Time)/(StartTime)))   #,label='Modelled Morphology')
         if (Time == PlotTime):
             #print (Time,StartTime)
             ax1.plot(X,Z,'-',lw=1.5,color=ColourMap((Time)/(StartTime)))             
             PlotTime -= PlotInterval
 
     
-    
-    N10Line = (NLines[-1].strip().split(" "))   #reading line -2?   
-    N10 = np.array(N10Line[1:],dtype="float64")
-    X2 = np.arange(0,len(N10))*0.1
-    mask = [N10!=N10[-1]]
-    N10 = N10[mask]
-    X2 = X2[mask]
+        N10Line = (NLines[-1].strip().split(" "))   #reading line -2?   
+        N10 = np.array(N10Line[1:],dtype="float64")
+        X2 = np.arange(0,len(N10))*0.1
+        mask = [N10!=N10[-1]]
+        N10 = N10[mask]
+        X2 = X2[mask]
 
-           
-    ax2.plot(X2,N10,'k-',lw=1.5, label='Modelled CRN Concentrations')
+        ax2.plot(X2,N10,'k-',lw=1.5) #, label='Modelled CRN Concentrations')
   
-        
-    #tweak the plot
-    #ax1.set_xticklabels([])
-
-    #Normalising CRN x positions to final cliff position
-    File2 = "../driver_files/Data/CB_CRN.data"
-    X,CRN,Error=np.loadtxt(File2,unpack=True,skiprows=1,usecols=(1,2,3),delimiter=" ")
-    NormalisedX = LCPosition - X
-    ax2.errorbar(NormalisedX,CRN,fmt='o', yerr=Error,c='grey', label='Measured CRN Concentrations')    
-    ax2.scatter(NormalisedX,CRN, s=0.1)
-
-    #File 3 uncorrected CRN data for Dunbar?
-    #File3 = "../driver_files/DB_CRN_uncorrected.data"
-    #X2,CRN2,Error2=np.loadtxt(File3,unpack=True,skiprows=1,usecols=(1,2,3),delimiter=" ")
-    #NormalisedX2 = LCPosition - X2
-    #ax2.errorbar(NormalisedX2,CRN2, fmt='o', yerr=Error2, c='k', label='Measured CRN Concentrations (uncorrected)')
-
-    #load the extracted shore platform profile (CB - profile read from lowest to highest Z)
-    #ExProfileName = "../driver_files/Data/Swath_Profile_CB.txt"
-    #Xprof, Zprof = np.loadtxt(ExProfileName, unpack=True,skiprows=1,usecols=(0,1))
-    #XCliffPosition = Xprof[-1]
-    #Xprof -= XCliffPosition
-    #Xprof += LCPosition
-
-    #load the extracted shore platform profile (SY - profile read from highest to lowest Z)
-    ExProfileName = "../driver_files/Data/CB_profile.txt"
-    Xprof, Zprof = np.loadtxt(ExProfileName, unpack=True,skiprows=1,usecols=(0,1))
-    #XCliffPosition = Xprof[0]
-    #Xprof -= XCliffPosition
-    Xprof = LCPosition - Xprof
     
-    #axis labels
-    ax1.set_ylabel("Elevation (m)")
-    ax2.set_ylabel("Concentration (a g$^-1$)")  #x 10$^3$ 
-    ax2.set_xlabel("Distance (m)")
-    #xmin, xmax = ax1.get_xlim()
-    #ax1.set_xlim(1200,1500)
-    #ax2.set_xlim(1200,1500) 
-    #ax1.set_ylim(-10,10)
-    #ax2.set_ylim(0,9000)
+#Normalising CRN x positions to final cliff position
+File2 = "../driver_files/Data/CB_CRN.data"
+X,CRN,Error=np.loadtxt(File2,unpack=True,skiprows=1,usecols=(1,2,3),delimiter=" ")
+NormalisedX = LCPosition - X
+ax2.errorbar(NormalisedX,CRN,fmt='o', yerr=Error,c='grey', label='Measured CRN Concentrations')    
+ax2.scatter(NormalisedX,CRN, s=0.1)
 
-    ax1.plot(Xprof,Zprof,'r-',lw=1.5, label='Extracted Morphology')  #NXprof for scalby, Xprof for CB
-
-   
-
-    ax1.legend(loc='upper left', numpoints=1)
-    ax2.legend(loc='lower left', numpoints=1)
+#load the extracted shore platform profile (SY - profile read from highest to lowest Z)
+ExProfileName = "../driver_files/Data/CB_profile.txt"
+Xprof, Zprof = np.loadtxt(ExProfileName, unpack=True,skiprows=1,usecols=(0,1))
+#XCliffPosition = Xprof[0]
+#Xprof -= XCliffPosition
+Xprof = LCPosition - Xprof
     
+#axis labels
+ax1.set_ylabel("Elevation (m)")
+ax2.set_ylabel("Concentration (a g$^-1$)")  #x 10$^3$ 
+ax2.set_xlabel("Distance (m)")
+#xmin, xmax = ax1.get_xlim()
+#ax1.set_xlim(1200,1500)
+#ax2.set_xlim(1200,1500) 
+#ax1.set_ylim(-10,10)
+#ax2.set_ylim(0,9000)
+ax1.plot(Xprof,Zprof,'r-',lw=1.5, label='Extracted Morphology')  #NXprof for scalby, Xprof for CB
 
-    fig1 = plt.gcf()
-    plt.show()
-    plt.draw()
-    #fig1.savefig('MCMC_SY_5_topo.png',dpi=300)
+ax1.legend(loc='upper left', numpoints=1)
+ax2.legend(loc='lower left', numpoints=1)
+    
+fig1 = plt.gcf()
+plt.show()
+plt.draw()
+#fig1.savefig('Dakota_result_test.png',dpi=300)
 
-#if __name__ == "__main__":
-    #FileName = "/Users/jennyshadrick/Dakota_Runs/Run_1","/Users/jennyshadrick/Dakota_Runs/Run_2" # /Users/jennyshadrick/RPM_JRS
-    #ColourMap = cm.gray
-    #make_plot(FileName,ColourMap)
         
