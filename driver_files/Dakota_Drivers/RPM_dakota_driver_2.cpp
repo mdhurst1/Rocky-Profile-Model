3828f5f53257164e404e9fb25cfa962c5c51d2ff
Jenny Shadrick <jrs17@ic.ac.uk>
2020-02-05 14:46:14 +0000

    changing driver to create outfiles

--- driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
+++ driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
@@ -136,9 +136,9 @@ int main(int nNumberofArgs,char *argv[])
 	double TidalRange = atof(argv[7]);
     double SubtidalEfficacy = atof(argv[8]);
     double WaveAttenuationConst = atof(argv[9]);
-    //double Resistance = pow(10,(atof(argv[10])));          //dakota varies FR on log scale
+    double Resistance = atof(argv[10]);          //dakota varies FR on log scale
     //double WeatheringRate = Resistance * pow(10,(atof(argv[11])));      //dakota varies K proportional to FR 0 - 0.5 range 
-	double Resistance = atof(argv[10]);
+	//double Resistance = atof(argv[10]);
 	double WeatheringRate = pow(10,(atof(argv[11]))); 
 	//double WeatheringRate = atof(argv[11]);
 	cout << "Resistance = " << Resistance << endl;
@@ -231,9 +231,9 @@ int main(int nNumberofArgs,char *argv[])
 	PlatformModel.InitialiseGeology(CliffHeight, CliffFailureDepth, Resistance, WeatheringRate, SubtidalEfficacy);
 
     // print initial condition to file - this is for testing - remove
-	//double TempTime = -9999;
-    //PlatformModel.WriteProfile(OutputFileName, TempTime);			
-	//if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
+	double TempTime = -9999;
+    PlatformModel.WriteProfile(OutputFileName, TempTime);			
+	if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
 
     //Loop through time
 	while (Time >= EndTime)
@@ -280,8 +280,8 @@ int main(int nNumberofArgs,char *argv[])
 		{
 			cout.flush();
 			cout << "RPM: Time " << setprecision(2) << fixed << Time << " years\r";
-			//PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
-            //if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
+			PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
+            if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
 			PrintTime -= PrintInterval;
 		}
 
