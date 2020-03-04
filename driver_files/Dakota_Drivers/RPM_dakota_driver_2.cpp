29f10db159d2125776fec50699bf046ebecedb7d
Jenny Shadrick <jrs17@ic.ac.uk>
2020-03-04 16:26:16 +0000

    changes for failed test

--- driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
+++ driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
@@ -453,7 +453,7 @@ int main(int nNumberofArgs,char *argv[])
    vector<double> NResiduals(NProfileData);
    vector<double> LResiduals(NProfileData);
    long double Likelihood = 1.L;
-   //double ZStd = 0.3;
+   double ZStd = 0.1;
    //double MaxTopo = Residuals[0];
    //double MinTopo = Residuals[0];
    
@@ -480,7 +480,7 @@ int main(int nNumberofArgs,char *argv[])
 	{
 		//Residuals calc for Likelihood
 	   LResiduals[i] = (ProfileZData[i]-TopoData[i])*(ProfileZData[i]-TopoData[i]);
-	   Likelihood *= fastexp(-(fabs(LResiduals[i])));     //(ZStd*ZStd));
+	   Likelihood *= fastexp(-(fabs(LResiduals[i])/(ZStd*ZStd)));     //(ZStd*ZStd));
 	}
 	//return Likelihood;
 
