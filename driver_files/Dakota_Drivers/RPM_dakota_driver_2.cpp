3f21b6dfce0020380821fdb98b446c8da72fe587
Jenny Shadrick <jrs17@ese-ajs13.ese.ic.ac.uk>
2020-07-16 09:56:13 +0100

    edits to dakota drivers

--- driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
+++ driver_files/Dakota_Drivers/RPM_dakota_driver_2.cpp
@@ -144,7 +144,7 @@ int main(int nNumberofArgs,char *argv[])
     double Resistance = pow(10,(atof(argv[10])));          //dakota varies FR on log scale
 	//double Resistance = atof(argv[10]);
 
-    //double WeatheringRate = Resistance * pow(10,(atof(argv[11])));      //dakota varies K proportional to FR 0 - 0.5 range 
+    //double WeatheringRate = Resistance * (atof(argv[11]));      //dakota varies K proportional to FR 0 - 0.5 range 
 	double WeatheringRate = pow(10,(atof(argv[11]))); 
 	//double WeatheringRate = atof(argv[11]);
 
@@ -239,9 +239,9 @@ int main(int nNumberofArgs,char *argv[])
 	PlatformModel.InitialiseGeology(CliffElevation, CliffFailureDepth, Resistance, WeatheringRate, SubtidalEfficacy);
 
     // print initial condition to file - this is for testing - remove
-	double TempTime = -9999;
-    PlatformModel.WriteProfile(OutputFileName, TempTime);			
-	if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
+	//double TempTime = -9999;
+    //PlatformModel.WriteProfile(OutputFileName, TempTime);			
+	//if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, TempTime);
 
     //Loop through time
 	while (Time >= EndTime)
@@ -288,8 +288,8 @@ int main(int nNumberofArgs,char *argv[])
 		{
 			cout.flush();
 			cout << "RPM: Time " << setprecision(2) << fixed << Time << " years\r";
-			PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
-            if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
+			//PlatformModel.WriteProfile(OutputFileName, Time);  //This is for testing - need to remove
+            //if (CRNFlag) PlatformCRN.WriteCRNProfile(OutputConcentrationFileName, Time);
 			PrintTime -= PrintInterval;
 		}
 
