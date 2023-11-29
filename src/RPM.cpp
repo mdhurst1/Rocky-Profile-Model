a84c4d6fc510d431aa5714c5ff7cabd2441d62bf
mdhurst1 <martin.d.hurst@gmail.com>
2023-11-29 22:14:01 +0000

    wrong param

--- src/RPM.cpp
+++ src/RPM.cpp
@@ -1260,7 +1260,7 @@ void RPM::WriteProfile(string OutputFileName, double Time, bool Print2Screen)
 		if (WriteCoastFile.is_open()) WriteCoastFile << MaxElevation << " " << MinElevation << " " << dZ << endl;
 		else
 		{
-			cout << "Unable to create file " << WriteCoastFile << endl;
+			cout << "Unable to create file " << OutputFileName << endl;
 			exit(0);
 		}
 	}
